26e0d351bf03a3bd4210f5e3742b548fbf4b1f5a
Ivuska <fischerova.ivka@gmail.com>
2023-03-07 14:40:07 +0100

    Destructuring of comp props and set onClick func.

--- src/components/Light/Light.jsx
+++ src/components/Light/Light.jsx
@@ -3,17 +3,15 @@ import './light.css';
 import bulbOn from './images/light-on.svg';
 import bulbOff from './images/light-off.svg';
 
-const Light = (props) => {
-    const { name, state } = props;
+const Light = ({ name, state }) => {
 
-
-    //const [stateVar, setState] = useState(state);
+    const [stateVar, setState] = useState(state);
 
     return (
         <>
-            <div className="light" /*onClick={() => setState(stateVar)}*/>
+            <div className="light" onClick={() => setState(!stateVar)}>
                 <div className="light__icon">
-                    <img src={state ? bulbOn : bulbOff} alt=""/>
+                    <img src={stateVar ? bulbOff : bulbOn} alt=""/>
                 </div>
                 <div className="light__name">
                     {name}
